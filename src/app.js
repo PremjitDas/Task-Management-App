5291a7483b6010a60892db9f61084d922f5c69f2
Premjit Das <dpremjit42@gmail.com>
2025-05-14 12:02:39 +0530

    change in  app file

--- src/app.js
+++ src/app.js
@@ -17,4 +17,11 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 
+import userRouter from "./routes/user.routes.js";
+import taskRouter from "./routes/task.routes.js"
+
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
+
 export { app };
